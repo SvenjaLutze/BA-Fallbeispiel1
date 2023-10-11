aecdbcdcb43a61b7ddf9b587fa8d434a9ba229de
SvenjaLutze <105362674+SvenjaLutze@users.noreply.github.com>
2023-10-11 08:21:47 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const showAllButton = document.getElementById("showAll");
+    const filteredImages = document.querySelectorAll(".filtered");
+    let allImagesVisible = false;
+
+    showAllButton.addEventListener("click", function () {
+        if (allImagesVisible) {
+            // Wenn alle Bilder sichtbar sind, ändere die Beschriftung des Buttons und blende die 10 Bilder aus
+            for (const image of filteredImages) {
+                image.style.display = "none";
+            }
+            showAllButton.textContent = "Alles anzeigen";
+            allImagesVisible = false;
+        } else {
+            // Wenn nicht alle Bilder sichtbar sind, ändere die Beschriftung des Buttons und zeige alle Bilder an
+            for (const image of filteredImages) {
+                image.style.display = "block";
+            }
+            showAllButton.textContent = "Weniger anzeigen";
+            allImagesVisible = true;
+        }
+    });
+});
