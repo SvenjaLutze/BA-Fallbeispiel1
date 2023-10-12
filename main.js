ff8abb885255a3d79c78233c5ba40474403fb272
SvenjaLutze <svenja.lutze@yahoo.de>
2023-10-12 15:39:19 +0200

    change alt attribute, add focus

--- main.js
+++ main.js
@@ -3,21 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const filteredImages = document.querySelectorAll(".filtered");
     let allImagesVisible = false;
 
+    var elements = document.getElementsByClassName(".filtered");
+    var requiredElement = elements[0];
+
     showAllButton.addEventListener("click", function () {
         if (allImagesVisible) {
-            // Wenn alle Bilder sichtbar sind, ändere die Beschriftung des Buttons und blende die 10 Bilder aus
             for (const image of filteredImages) {
                 image.style.display = "none";
             }
             showAllButton.textContent = "Mehr Bilder anzeigen";
             allImagesVisible = false;
         } else {
-            // Wenn nicht alle Bilder sichtbar sind, ändere die Beschriftung des Buttons und zeige alle Bilder an
             for (const image of filteredImages) {
                 image.style.display = "block";
             }
             showAllButton.textContent = "Weniger Bilder anzeigen";
             allImagesVisible = true;
+
+            requiredElement.focus();
         }
     });
 });
